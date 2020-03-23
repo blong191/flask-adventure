2b9ea0becd964c85c3a34d309c77f9c130926d9c
Benjamin <39armyguy@gmail.com>
2020-03-23 16:22:21 -0400

    aaaappppp

--- adventure/__init__.py
+++ adventure/__init__.py
@@ -1,19 +1,6 @@
 from flask import Flask
+app = Flask(__name__)
 
-def create_app(test_config=None):
-    app = Flask(__name__)
-    app.config.from_mapping(
-        SECRET_KEY='dev'
-    )
-
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
-
-
-    @app.route('/')
-    def index():
-        return 'Flask Heroku Demo'
-
-    return app
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
