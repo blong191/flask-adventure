f9cbcc9f5c9dde9b2c249886de5fd2dc5347c488
Benjamin <39armyguy@gmail.com>
2020-03-26 15:38:07 -0400

    the play.html page now reads the current sceney

--- flaskr/game.py
+++ flaskr/game.py
@@ -10,4 +10,17 @@ def index():
 
 @bp.route('/start')
 def start():
-    return render_template('play.html')
+
+    current_scene = {
+    'place': 'The Village',
+    'output': """
+        :o-|-[
+        You awaken to a sound of crumpling leaves.
+        infront of you is a man waring a top hat.
+        "Hello there my boy I'm sorry to awaken but this is not a charity house"
+        you realise that you fell aslep out side the store.
+        you apologise to the man and stand up only to get smashed into the wall by a skeloton
+        what will you do?
+        """
+    }
+    return render_template('play.html',scene=current_scene)
