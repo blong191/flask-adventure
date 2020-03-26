1d21140976d841894af5db6caed110cc4f27f509
Benjamin <39armyguy@gmail.com>
2020-03-26 16:07:02 -0400

    added new scens death and campfire and got the branching paths started

--- flaskr/game.py
+++ flaskr/game.py
@@ -8,7 +8,7 @@ bp = Blueprint('game', __name__)
 def index():
     return render_template('game.html')
 
-@bp.route('/start')
+@bp.route('/start', methods=('GET', 'POST' ))
 def start():
 
     current_scene = {
@@ -21,6 +21,28 @@ def start():
         you realise that you fell aslep out side the store.
         you apologise to the man and stand up only to get smashed into the wall by a skeloton
         what will you do?
+        (for the demo just skip it)
         """
     }
+    if request.method == "POST":
+        choice = request.form['action']
+        if choice == 'skip':
+            return redirect(url_for('game.campfire'))
+        elif choice == 'die':
+            return redirect(url_for('gmae.death'))
     return render_template('play.html',scene=current_scene)
+
+@bp.route('/death', methods=( 'GET','POST'))
+def death():
+    current_scene = {
+    'place':'hell',
+    'output': 'GET good ... retry (You have to refresh)'
+    }
+
+@bp.route('/campfire', methods=('GET','POST'))
+def campfire():
+    current_scene = {
+    'place':'campfire',
+    'output':'end of demo'
+    }
+    return render_template('play.html', scene=current_scene)
